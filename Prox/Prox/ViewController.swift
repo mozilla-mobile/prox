a3fa4723395f68ecd9682cef6adaeb87f6ca3063
Emily Toop <etoop@mozilla.com>
2016-10-04 15:04:24 +0100

    Initial commit of empty Xcode project for the Prox app

---
+++ Prox/Prox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prox
+//
+//  Created by Emily Toop on 04/10/2016.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
