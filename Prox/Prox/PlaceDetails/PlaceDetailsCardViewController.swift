810eae7fd1d8b1bdf41fd5ce802d8b2c208dcc30
Emily Toop <etoop@mozilla.com>
2016-11-01 15:40:58 +0000

    Issue 62 - Update background image as user scrolls through image carousel and when user pages to new place card

--- Prox/Prox/PlaceDetails/PlaceDetailsCardViewController.swift
+++ Prox/Prox/PlaceDetails/PlaceDetailsCardViewController.swift
@@ -6,6 +6,10 @@ import UIKit
 
 private let  PlaceDetailsCardCellReuseIdentifier = "ImageCarouselCell"
 
+protocol PlaceDetailsImageDelegate: class {
+    func imageCarousel(imageCarousel: UIView, placeImageDidChange newImageURL: URL)
+}
+
 class PlaceDetailsCardViewController: UIViewController {
 
     var place: Place! {
@@ -13,6 +17,9 @@ class PlaceDetailsCardViewController: UIViewController {
             setPlace(place: place)
         }
     }
+
+    weak var placeImageDelegate: PlaceDetailsImageDelegate?
+
     lazy var cardView: PlaceDetailsCardView = {
         let view = PlaceDetailsCardView()
         return view
@@ -101,6 +108,10 @@ class PlaceDetailsCardViewController: UIViewController {
         let pageSize = imageCarousel.bounds.size
         let xOffset = pageSize.width * CGFloat(pageControl.currentPage)
         imageCarouselCollectionView.setContentOffset(CGPoint(x: xOffset, y: 0), animated: true)
+        if let imageURLString = place.photoURLs?[pageControl.currentPage],
+            let imageURL = URL(string: imageURLString) {
+            placeImageDelegate?.imageCarousel(imageCarousel: imageCarousel, placeImageDidChange: imageURL)
+        }
     }
 }
 
@@ -142,5 +153,10 @@ extension PlaceDetailsCardViewController: UIScrollViewDelegate {
         let pageSize = imageCarouselCollectionView.bounds.size
         let selectedPageIndex = Int(floor((scrollView.contentOffset.x-pageSize.width/2)/pageSize.width))+1
         pageControl.currentPage = Int(selectedPageIndex)
+
+        if let imageURLString = place.photoURLs?[selectedPageIndex],
+            let imageURL = URL(string: imageURLString) {
+            placeImageDelegate?.imageCarousel(imageCarousel: imageCarousel, placeImageDidChange: imageURL)
+        }
     }
 }
