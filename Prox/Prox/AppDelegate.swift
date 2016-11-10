981e2160f255255ac3298efda63c3ed118028a8f
Emily Toop <etoop@mozilla.com>
2016-11-10 15:29:24 +0000

    Issue 125 - restart time at location timer when app moves to foreground

--- Prox/Prox/AppDelegate.swift
+++ Prox/Prox/AppDelegate.swift
@@ -62,6 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        placeCarouselViewController?.startTimeAtLocationTimer()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
