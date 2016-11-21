58288c1a240bce6990629e3c3c58cac282ede238
Michael Comella <michael.l.comella@gmail.com>
2016-11-21 13:45:19 -0800

    Issue 178: RM unnecessary categories filtering code.
    The UX list of places is exhaustive so whitelisting the places UX said
    to show is as good as blacklisting the places they said to hide. The
    server already uses the whitelist to query yelp (to return fewer results)
    so no actions are necessary on the client to handle the top level
    categories they specified.
    
    ty to liuche for pointing this out.

--- Prox/Prox/Utilities/CategoriesUtil.swift
+++ Prox/Prox/Utilities/CategoriesUtil.swift
@@ -6,51 +6,11 @@ import Foundation
 
 struct CategoriesUtil {
 
-    static let HiddenRootCategories = Set(arrayLiteral: "auto", // 🚗
-                                                  "bicycles",
-                                                  "education",
-                                                  "financialservices",
-                                                  "health",
-                                                  "homeservices",
-                                                  "localservices",
-                                                  "professional",
-                                                  "realestate",
-                                                  "religiousorgs")
-
     private static let AllCategoriesPath = "Data.bundle/yelp_categories_v3"
     private static let AllCategoriesExt = "json"
 
     static func shouldShowPlace<S : Sequence>(byCategories categories: S) -> Bool where S.Iterator.Element == String {
-        // Hide if all match.
-        let rootCategories: Set<String>
-        do {
-            rootCategories = try getRootCategories(forCategories: categories)
-        } catch CategoryError.Unknown(let name) {
-            print("lol unknown category name, \(name) - filtering out by category")
-            return false
-        } catch { // I don't know why this is necessary - afaik, no other errors are thrown.
-            print("lol Unknown error occurred while filtering categories - filtering out")
-            return false
-        }
-        let allCategoriesMatch = rootCategories.subtracting(HiddenRootCategories).isEmpty
-        return !allCategoriesMatch
-    }
-
-    // This accesses the app bundle, which could be slow. If it's an issue, consider using a background thread.
-    static let categoryToParentsMap = getCategoryToParentsMap()
-
-    private static func getCategoryToParentsMap() -> [String:Set<String>] {
-        let json = loadAllCategoriesFile()
-
-        var categoryToParents = [String:Set<String>]()
-        for categoryObject in json {
-            let obj = categoryObject as! NSDictionary
-            let title = obj["alias"] as! String
-            let parents = obj["parents"] as! [String]
-
-            categoryToParents[title] = Set(parents)
-        }
-        return categoryToParents
+        return true // we'll update this in the next commits.
     }
 
     private static func loadAllCategoriesFile() -> NSArray {
@@ -69,27 +29,4 @@ struct CategoriesUtil {
         return try! JSONSerialization.jsonObject(with: inputStream) as! NSArray
     }
 
-    static func getRootCategories<S : Sequence>(forCategories categories: S) throws -> Set<String> where S.Iterator.Element == String {
-        return try categories.reduce(Set()) { res, category in
-            res.union(try getRootCategories(forCategory: category))
-        }
-    }
-
-    private static func getRootCategories(forCategory category: String) throws -> Set<String> {
-        guard let parents = categoryToParentsMap[category] else {
-            throw CategoryError.Unknown(name: category)
-        }
-
-        if parents.isEmpty {
-            return Set([category])
-        }
-
-        return try parents.reduce(Set()) { res, parent in
-            res.union(try getRootCategories(forCategory: parent))
-        }
-    }
-}
-
-enum CategoryError: Error {
-    case Unknown(name: String)
 }
