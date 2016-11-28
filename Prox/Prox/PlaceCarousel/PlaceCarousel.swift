4c8b1a3dc97224cdd0cbe314670f9aeda26853fd
Michael Comella <michael.l.comella@gmail.com>
2016-11-28 10:35:05 -0800

    Issue #212: No data available -> No data.
    From antlam's comment:
    https://github.com/mozilla-mobile/prox/issues/212#issuecomment-262839429

--- Prox/Prox/PlaceCarousel/PlaceCarousel.swift
+++ Prox/Prox/PlaceCarousel/PlaceCarousel.swift
@@ -108,8 +108,8 @@ extension PlaceCarousel: UICollectionViewDataSource {
 
         downloadAndSetImage(for: place, into: cell)
 
-        PlaceUtilities.updateReviewUI(fromProvider: place.yelpProvider, onView: cell.yelpReview, isTextShortened: true)
-        PlaceUtilities.updateReviewUI(fromProvider: place.tripAdvisorProvider, onView: cell.tripAdvisorReview, isTextShortened: true)
+        PlaceUtilities.updateReviewUI(fromProvider: place.yelpProvider, onView: cell.yelpReview)
+        PlaceUtilities.updateReviewUI(fromProvider: place.tripAdvisorProvider, onView: cell.tripAdvisorReview)
 
         PlaceUtilities.updateTravelTimeUI(fromPlace: place, toLocation: locationProvider?.getCurrentLocation(), forView: cell)
 
