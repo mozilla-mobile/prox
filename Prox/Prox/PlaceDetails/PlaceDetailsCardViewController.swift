aa785312e933302dc94d59022f7457ee7ea51090
Stephan Leroux <sleroux@mozilla.com>
2016-11-29 13:05:26 -0500

    Issue 333 - Fixed potential divide-by-0 crash when image carousel is empty

--- Prox/Prox/PlaceDetails/PlaceDetailsCardViewController.swift
+++ Prox/Prox/PlaceDetails/PlaceDetailsCardViewController.swift
@@ -295,6 +295,12 @@ extension PlaceDetailsCardViewController: UIScrollViewDelegate {
 
     fileprivate func didChangePage(scrollView: UIScrollView) {
         let pageSize = imageCarouselCollectionView.bounds.size
+
+        // Unable to change pages if we don't have anything to page!
+        guard pageSize != CGSize.zero && pageSize.width != 0 else {
+            return
+        }
+        
         let selectedPageIndex = Int(floor((scrollView.contentOffset.x-pageSize.width/2)/pageSize.width))+1
         pageControl.currentPage = selectedPageIndex
 
