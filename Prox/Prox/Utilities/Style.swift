9e956f9ee6575b093b612149bf9825b6baf3ed21
Michael Comella <michael.l.comella@gmail.com>
2017-02-22 15:06:25 -0800

    No issue: move cardView style to separate file.

---
+++ Prox/Prox/Utilities/Style.swift
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+struct Style {
+    static let cardViewCornerRadius: CGFloat = 10
+    static let cardViewShadowRadius: CGFloat = 5
+    static let cardViewShadowOffset = CGSize(width: 0, height: 2)
+    static let cardViewShadowOpacity: Float = 0.4
+}
